ed9629b86e0dd41c0d0e1a0479b50fe8c717ca1b
Sonali-Ambedare-amla <114081324+Sonali-Ambedare-amla@users.noreply.github.com>
2023-05-25 16:26:44 +0530

    Source Link Demo

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,13 @@
+﻿using LibNewCalculation;
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation cs = new Calculation();
+            cs.Addition(2, 4);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
